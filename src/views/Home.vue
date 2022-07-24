da5b7a2847b7d3f4d4afbba09b8969671433bc43
Kerstin Neininger <kerstin.neininger@gmail.com>
2022-07-24 12:52:32 +0200

    chore: add version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,64 @@
 <template>
-    <v-container fluid grid-list-lg>
-      <!-- Logout button -->
-      <v-col class="text-right">
-        <v-btn text small color="black" @click="logout">
-          Logout
-          <v-icon right>fas fa-power-off</v-icon>
+  <v-container fluid grid-list-lg>
+    <!-- Logout button -->
+    <v-col class="text-right">
+      <v-btn text small color="black" @click="logout">
+        Logout
+        <v-icon right>fas fa-power-off</v-icon>
+      </v-btn>
+    </v-col>
+
+    <!-- Navigation -->
+    <!-- <v-card>-->
+    <v-navigation-drawer
+      v-model="drawer"
+      dark
+      app
+      :mini-variant.sync="mini"
+      permanent
+    >
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <img :src="require('../assets/logo.png')" />
+        </v-list-item-avatar>
+
+        <v-list-item-title class="text-h6">Methodikum</v-list-item-title>
+
+        <v-btn icon @click.stop="mini = !mini">
+          <v-icon>mdi-chevron-left</v-icon>
         </v-btn>
-      </v-col>
+      </v-list-item>
+      <p class="version">&copy; {{ this.year }} -- Version v1.0.0</p>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          :to="`${item.route}`"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
-      <!-- Navigation -->
-      <!-- <v-card>-->
-      <v-navigation-drawer
-        v-model="drawer"
-        dark
-        app
-        :mini-variant.sync="mini"
-        permanent
-      >
-        <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <img :src="require('../assets/logo.png')" />
-          </v-list-item-avatar>
-
-          <v-list-item-title class="text-h6">Methodikum</v-list-item-title>
-
-          <v-btn icon @click.stop="mini = !mini">
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
         </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!--</v-card>-->
 
-        <v-divider></v-divider>
-        <v-list dense nav>
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-            :to="`${item.route}`"
-          >
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <!--</v-card>-->
-      <v-main>
-        <v-container fluid grid-list-lg pa-10>
-          <v-layout justify-center>
-            <v-fade-transition mode="out-in">
-              <router-view></router-view>
-            </v-fade-transition>
-          </v-layout>
-        </v-container>
-      </v-main>
-    </v-container>
+    <v-main>
+      <v-container fluid grid-list-lg pa-10>
+        <v-layout justify-center>
+          <v-fade-transition mode="out-in">
+            <router-view></router-view>
+          </v-fade-transition>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-container>
 </template>
 
 <script>
@@ -74,7 +75,11 @@ export default {
       mini: false,
     };
   },
-  computed: {},
+  computed: {
+    year() {
+      return new Date().getFullYear();
+    },
+  },
   created() {
     // load tab content
     const path = "/welcome";
@@ -90,4 +95,10 @@ export default {
 </script>
 
 <style>
+.version {
+  text-align: center;
+  font-family: "Oswald", sans-serif;
+  font-size: 11px;
+  color: #fff;
+}
 </style>
